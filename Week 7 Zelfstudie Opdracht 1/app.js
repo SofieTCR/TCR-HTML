935aee50ff6acd2296cd42a66e9084cf7d9091b6
Sofie <sofiebrink@yahoo.com>
2022-10-13 11:22:08 +0200

    Opdracht 5.1

---
+++ Week 7 Zelfstudie Opdracht 1/app.js
@@ -0,0 +1,9 @@
+
+var TestP = document.getElementById("TestId");
+
+TestP.innerHTML = "De Pagina is " + window.innerWidth.toString() + "px breed.";
+
+
+function UpdateParagraaf() {
+    TestP.innerHTML = "De Pagina is " + window.innerWidth.toString() + "px breed.";
+}
